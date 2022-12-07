625335d727a9ba675026c1771c12438627a95e75
JPabisz <64328131+JPabisz@users.noreply.github.com>
2022-12-06 20:47:32 -0500

    Update and rename hello.hs to Main.hs

---
+++ Main.hs
@@ -0,0 +1,11 @@
+{-
+ - Author: James Pabisz, Jpabisz2020@my.fit.edu
+ - Author: Remington Greko, Rgreko2020@my.fit.edu
+ - Course: CSE 4250, Fall 2021
+ - Project: Proj4, Tautology Checker
+ - Language implementation: Glorious Glasgow Haskell Compilation System, version 8.6.5
+ -}
+
+
+main = putStrLn "Hello World"
+
