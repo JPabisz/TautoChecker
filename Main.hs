4ac45785c690d95b374b9d3b5bf7a022651c2454
JPabisz <64328131+JPabisz@users.noreply.github.com>
2022-12-06 23:05:05 -0500

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -7,5 +7,35 @@
  -}
 
 
-main = putStrLn "Hello World"
+{-
+ - Author: James Pabisz, Jpabisz2020@my.fit.edu
+ - Author: Remington Greko, Rgreko2020@my.fit.edu
+ - Course: CSE 4250, Fall 2021
+ - Project: Proj4, Tautology Checker
+ - Language implementation: Glorious Glasgow Haskell Compilation System, version 8.6.5
+ -}
+ 
+module Main where
 
+    data Proposition =
+          Const Bool
+        | Var String
+        | A Proposition Proposition
+        | C Proposition Proposition
+        | D Proposition Proposition
+        | E Proposition Proposition
+        | J Proposition Proposition
+        | K Proposition Proposition
+        | N Proposition
+        deriving (Eq, Show)
+        
+    concatProp :: Proposition -> [String]
+    concatProp (Const  _   ) = []
+    concatProp (Var    x   ) = [x]
+    concatProp (A    prop1 p2) = (concatProp prop1) ++ (concatProp p2)
+    concatProp (C    prop1 p2) = (concatProp prop1) ++ (concatProp p2)
+    concatProp (D    prop1 p2) = (concatProp prop1) ++ (concatProp p2)
+    concatProp (E    prop1 p2) = (concatProp prop1) ++ (concatProp p2)
+    concatProp (J    prop1 p2) = (concatProp prop1) ++ (concatProp p2)
+    concatProp (K    prop1 p2) = (concatProp prop1) ++ (concatProp p2)
+    concatProp (N    prop) = concatProp proposition
