544252301aeb5e83909b950a2fa5063c370d363f
JPabisz <64328131+JPabisz@users.noreply.github.com>
2022-12-06 23:05:24 -0500

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -5,15 +5,6 @@
  - Project: Proj4, Tautology Checker
  - Language implementation: Glorious Glasgow Haskell Compilation System, version 8.6.5
  -}
-
-
-{-
- - Author: James Pabisz, Jpabisz2020@my.fit.edu
- - Author: Remington Greko, Rgreko2020@my.fit.edu
- - Course: CSE 4250, Fall 2021
- - Project: Proj4, Tautology Checker
- - Language implementation: Glorious Glasgow Haskell Compilation System, version 8.6.5
- -}
  
 module Main where
 
